00a2672e47ecd70233c19638864e3adf04469b5c
azekeprofit <64343918+azekeprofit@users.noreply.github.com>
2020-08-24 14:34:04 +0600

    rewrite

--- youtube multi.user.js
+++ youtube multi.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Youtube Multi
-// @version        0.2
+// @version        0.3
 // @description    Adds additional Youtube subtitles alongside the one you chose, presuming video has them and that all lines are synced between versions
 // @include        https://www.youtube.com/watch?v=*
 // @grant          none
@@ -11,11 +11,11 @@ const myLangs='en,kk,ko'; // list all languages you want to be shown, languages 
 
 if(location.href.startsWith('https://www.youtube.com/watch?v=')){
 
-setTimeout(()=>{
-const controls=document.querySelector('.ytp-left-controls');
+    const langHead = 'youtube-multi-lang';
 
-const bookmarklet=`function(...langs){
-const langHead='youtube-multi-lang';
+setTimeout(()=>{
+    const controls = document.querySelector('.ytp-left-controls');
+    const bookmarklet = `function(langHead,...langs){
 
 if(window.youtubeMultiLangCaptionsIntervalCode){
 clearInterval(window.youtubeMultiLangCaptionsIntervalCode);
@@ -24,23 +24,23 @@ document.querySelector('.caption-window.ytp-caption-window-bottom .captions-text
 
 return;
 }
-    let subs=window.youtubeMultiLangCaptions=window.youtubeMultiLangCaptions||[];
+    let subs=window.youtubeMultiLangCaptions=window.youtubeMultiLangCaptions||new Map();
     (window.youtubeMultiLangCaptions.length?Promise.resolve():
 
 Promise.all(document.getElementById('movie_player').getPlayerResponse().captions.playerCaptionsTracklistRenderer.captionTracks.flatMap(c =>
             langs.map((lang, i) => {
                             const {baseUrl,vssId}=c;
                             if(vssId == lang || vssId.startsWith(lang + '-')){
-                            const newSub = subs[i] = {times:[],lines:[],durs:[]};
+                            const newSub = [];
+                            subs.set(i, newSub);
                             return fetch(baseUrl).then(r => r.text()).then(x => {
-                                const {times,lines,durs} = newSub;
+                                const {times,lines,ends} = newSub;
                                 new DOMParser().parseFromString(x.replace(/&amp;/g, '&'), 'text/xml').querySelectorAll('text').forEach(l => {
-                                    times.push(l.getAttribute('start'));
-                                    durs.push(l.getAttribute('dur'));
-                                    lines.push(l.innerHTML)
+                                    const start=l.getAttribute('start')-0;
+                                    newSub.push({start, end: l.getAttribute('dur')-0+start, html: l.innerHTML, index: newSub.length});
                                 })})}})))
-    
-).then(()=>window.youtubeMultiLangCaptionsIntervalCode=setInterval(()=>{
+
+).then(() =>window.youtubeMultiLangCaptionsIntervalCode=setInterval(()=>{
         const caps = document.querySelector('.caption-window.ytp-caption-window-bottom');
         if (!caps) return;
         const curTime = document.querySelector('video').getCurrentTime();
@@ -49,27 +49,24 @@ Promise.all(document.getElementById('movie_player').getPlayerResponse().captions
         lines.classList.add(langHead);
         const line = lines.querySelector('.caption-visual-line');
 
-        subs.forEach((sub,i)=>{if(sub){
-          const langClass=langHead+i;
-          if([...lines.querySelectorAll('.'+langClass+':last-of-type')].every(n=>sub.times[n.dataset.lineIndex-0+1]&&sub.times[n.dataset.lineIndex-0+1]<=curTime)){
-          const oldLines=new Set([...lines.querySelectorAll('.'+langClass)].map(l=>l.dataset.lineIndex-0));
-          const linesToDelete=new Set([...oldLines]);
+        subs.forEach((sub, i) => {
+            const langClass = langHead +i;
 
-          let lineIndex = -1; let time;
-          while(time=sub.times[++lineIndex])
-              if(time <= curTime && curTime <= time-0 + (sub.durs[lineIndex]-0)) {
-                   linesToDelete.delete(lineIndex);
-                   if(!oldLines.has(lineIndex)){
-                   const newLine=line.cloneNode(true);
-                   newLine.dataset.lineIndex=lineIndex;
-                   newLine.classList.add(langHead,langClass,langClass+'-line'+lineIndex);
-                   newLine.querySelector('.ytp-caption-segment').innerHTML = sub.lines[lineIndex];
-                   lines.appendChild(newLine)}};
-              if(linesToDelete.size)lines.querySelectorAll([...linesToDelete].map(l=>'.'+langClass+'-line'+l).join(',')).forEach(n=>n.remove());
-         }}})},100));}`;
+            const oldLines = new Set([...lines.querySelectorAll('.'+langClass)].map(l=>l.dataset.lineIndex -0));
+            const linesToDelete = new Set([...oldLines]);
 
+            sub.filter(({start,end})=>start<=curTime && curTime<=end).forEach(({html,index}) => {
+                      linesToDelete.delete(index);
+                      if (!oldLines.has(index)) {
+                          const newLine = line.cloneNode(true);
+                          newLine.dataset.lineIndex = index;
+                          newLine.classList.add(langHead, langClass, langClass + '-line' +index);
+                          newLine.querySelector('.ytp-caption-segment').innerHTML = html;
+                          lines.appendChild(newLine)}});
+            if (linesToDelete.size) lines.querySelectorAll([...linesToDelete].map(l=>'.' +langClass + '-line' +l).join(',')).forEach(n=>n.remove());
+          })}, 100)); }`;
 
-const bookmarkletAddress=`javascript:(${bookmarklet})(${myLangs.split(',').map(t=>'".'+t.toLowerCase()+'"')}),void(0)`;
+const bookmarkletAddress=`javascript:(${bookmarklet})('${langHead}',${myLangs.split(',').map(t=>'".'+t.toLowerCase()+'"')}),void(0)`;
 
 function addElement(t,p){
 var r=(p||document.body).appendChild(document.createElement(t));
@@ -80,9 +77,9 @@ return r;}
 addElement('a',controls,'innerText','Multi Lang','title','Shows multiple Youtube subtitles at once','href',bookmarkletAddress);
 
 addElement('style',0,'innerHTML',`
-.captions-text .caption-visual-line.youtube-multi-lang {display: none !important;}
-.captions-text.youtube-multi-lang .caption-visual-line {display: none !important;}
-.captions-text.youtube-multi-lang .caption-visual-line.youtube-multi-lang {display: block !important;}`);
+.captions-text .caption-visual-line.${langHead} {display: none !important;}
+.captions-text.${langHead} .caption-visual-line {display: none !important;}
+.captions-text.${langHead} .caption-visual-line.${langHead} {display: block !important;}`);
 
 },700);
 }})()

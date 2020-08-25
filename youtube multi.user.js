882d83e870175861a914edaffda546421477723f
azekeprofit <64343918+azekeprofit@users.noreply.github.com>
2020-08-25 08:37:02 +0600

    bookmarklet function, timing fix

--- youtube multi.user.js
+++ youtube multi.user.js
@@ -15,73 +15,75 @@ if(location.href.startsWith('https://www.youtube.com/watch?v=')){
 
     setTimeout(()=>{
     const controls = document.querySelector('.ytp-left-controls');
-    const bookmarklet = `function(langHead,...langs){
 
-    const videoPlayer=document.querySelector('#movie_player');
+    const bookmarkletFunction=function (langHead,...langs){
+      
+      const videoPlayer=document.querySelector('#movie_player');
+  
+      if(window.youtubeMultiLangCaptionsIntervalCode){
+        clearInterval(window.youtubeMultiLangCaptionsIntervalCode);
+        window.youtubeMultiLangCaptionsIntervalCode='';
+  
+        videoPlayer.classList.remove(langHead);
+        return;
+      }
+  
+      const classes=langs.map((l, i) =>'.'+langHead+i);
+      const sortClasses=langs.map((l, i) =>classes.slice(i+1).join(','));
+  
+      sortClasses[0]='.caption-visual-line';
+      sortClasses[langs.length-1]=':not(*)';
+  
+      function div(classes, parent, insertBefore) {
+        var el=parent.querySelector(classes);
+        if(el)return el;
+        el=document.createElement('div');
+        el.classList.add(...classes.split('.').filter(c=>c));
+        parent.insertBefore(el, insertBefore);
+        return el;
+      }
+  
+      let subs=window.youtubeMultiLangCaptions=window.youtubeMultiLangCaptions||new Map();
+      (window.youtubeMultiLangCaptions.length?Promise.resolve():
+  
+      Promise.all(videoPlayer.getPlayerResponse().captions.playerCaptionsTracklistRenderer.captionTracks.flatMap(({baseUrl, vssId}) =>
+              langs.map((lang, langIndex) => {
+                              if(vssId == lang || vssId.startsWith(lang + '-')){
+                              const newSub = [];
+                              subs.set(langIndex, newSub);
+                              return fetch(baseUrl).then(r => r.text()).then(x => {
+                                  new DOMParser().parseFromString(x.replace(/&amp;/g, '&'), 'text/xml').querySelectorAll('text').forEach(l => {
+                                      const start=l.getAttribute('start')-0;
+                                      if(newSub.length) {
+                                        const prevSub=newSub[newSub.length-1];
+                                        if(prevSub.start==prevSub.end)
+                                          prevSub.end=start; }
+                                          if(l.innerHTML.trim().length)
+                                          newSub.push({start, end: l.getAttribute('dur')-0+start, html: l.innerHTML, index: newSub.length});
+                                  })})}})))
+      ).then(() =>window.youtubeMultiLangCaptionsIntervalCode=setInterval(()=>{
+          subs.forEach((sub, langIndex) => {
+              const langClass = langHead+langIndex;
+  
+              let caps = div('.caption-window.ytp-caption-window-bottom.'+langHead, videoPlayer);
+  
+              const curTime = document.querySelector('video').getCurrentTime();
+              const lines = div('.captions-text', caps);
+  
+              const oldLines = new Set([...lines.querySelectorAll('.'+langClass)].map(l=>l.dataset.lineIndex-0));
+  
+              sub.forEach(({start,end,html,index}) => {
+                        if(start<=curTime && curTime<end)
+                        if (!oldLines.delete(index)) {
+                            const newLine = div('.caption-visual-line.'+langClass+'.'+langClass+'-line'+index, lines, caps.querySelector(sortClasses[langIndex]));
+                            newLine.dataset.lineIndex = index;
+                            div('.ytp-caption-segment', newLine).innerHTML = html;
+                            }});
+              if (oldLines.size) lines.querySelectorAll([...oldLines].map(l=>'.' +langClass + '-line' +l).join(',')).forEach(n=>n.remove());
+           })},100)
+      ).then(()=>videoPlayer.classList.add(langHead))};
 
-    if(window.youtubeMultiLangCaptionsIntervalCode){
-      clearInterval(window.youtubeMultiLangCaptionsIntervalCode);
-      window.youtubeMultiLangCaptionsIntervalCode='';
-
-      videoPlayer.classList.remove(langHead);
-      return;
-    }
-
-    const classes=langs.map((l, i) =>'.'+langHead+i);
-    const sortClasses=langs.map((l, i) =>classes.slice(i+1).join(','));
-
-    sortClasses[0]='.caption-visual-line';
-    sortClasses[langs.length-1]=':not(*)';
-
-    function div(classes, parent, insertBefore) {
-      var el=parent.querySelector(classes);
-      if(el)return el;
-      el=document.createElement('div');
-      el.classList.add(...classes.split('.').filter(c=>c));
-      parent.insertBefore(el, insertBefore);
-      return el;
-    }
-
-    let subs=window.youtubeMultiLangCaptions=window.youtubeMultiLangCaptions||new Map();
-    (window.youtubeMultiLangCaptions.length?Promise.resolve():
-
-    Promise.all(videoPlayer.getPlayerResponse().captions.playerCaptionsTracklistRenderer.captionTracks.flatMap(({baseUrl, vssId}) =>
-            langs.map((lang, langIndex) => {
-                            if(vssId == lang || vssId.startsWith(lang + '-')){
-                            const newSub = [];
-                            subs.set(langIndex, newSub);
-                            return fetch(baseUrl).then(r => r.text()).then(x => {
-                                new DOMParser().parseFromString(x.replace(/&amp;/g, '&'), 'text/xml').querySelectorAll('text').forEach(l => {
-                                    const start=l.getAttribute('start')-0;
-                                    if(newSub.length) {
-                                      const prevSub=newSub[newSub.length-1];
-                                      if(prevSub.start==prevSub.end)
-                                        prevSub.end=start; }
-                                        if(l.innerHTML.trim().length)
-                                        newSub.push({start, end: l.getAttribute('dur')-0+start, html: l.innerHTML, index: newSub.length});
-                                })})}})))
-
-).then(() =>window.youtubeMultiLangCaptionsIntervalCode=setInterval(()=>{
-        subs.forEach((sub, langIndex) => {
-            const langClass = langHead+langIndex;
-
-            let caps = div('.caption-window.ytp-caption-window-bottom.'+langHead, videoPlayer);
-
-            const curTime = document.querySelector('video').getCurrentTime();
-            const lines = div('.captions-text', caps);
-
-            const oldLines = new Set([...lines.querySelectorAll('.'+langClass)].map(l=>l.dataset.lineIndex-0));
-
-            sub.filter(({start,end})=>start<=curTime && curTime<=end).forEach(({html,index}) => {
-                      if (!oldLines.delete(index)) {
-                          const newLine = div('.caption-visual-line.'+langClass+'.'+langClass+'-line'+index, lines, caps.querySelector(sortClasses[langIndex]));
-                          newLine.dataset.lineIndex = index;
-                          div('.ytp-caption-segment', newLine).innerHTML = html;
-                          }});
-            if (oldLines.size) lines.querySelectorAll([...oldLines].map(l=>'.' +langClass + '-line' +l).join(',')).forEach(n=>n.remove());
-    }) }, 100)).then(() =>videoPlayer.classList.add(langHead))}`;
-
-const bookmarkletAddress=`javascript:(${bookmarklet})('${langHead}',${myLangs.split(',').map(t=>'".'+t.toLowerCase()+'"')}),void(0)`;
+const bookmarkletAddress=`javascript:(${bookmarkletFunction.toString()})('${langHead}',${myLangs.split(',').map(t=>'".'+t.toLowerCase()+'"')}),void(0)`;
 
 function addElement(t,p){
 var r=(p||document.body).appendChild(document.createElement(t));
@@ -92,13 +94,16 @@ return r;}
 addElement('a', controls, 'innerText', 'Multi Lang', 'class', langHead+'-toggle', 'title', 'Shows multiple Youtube subtitles at once', 'href', bookmarkletAddress);
 
 addElement('style', 0, 'innerHTML', `
-.caption-window.${langHead} { bottom: 2%; left: 30%; right: 30%; text-align: center; font-size: 21.3333px; color: rgb(255, 255, 255); font-family: "YouTube Noto", Roboto, "Arial Unicode Ms", Arial, Helvetica, Verdana, "PT Sans Caption", sans-serif; }
-.caption-window.${langHead} .captions-text {display: inline-block; background: rgba(8, 8, 8, 0.75); fill: rgb(255, 255, 255); }
 .caption-window.${langHead} {display: none !important; }
 #movie_player.${langHead} .caption-window {display: none !important; }
 #movie_player.${langHead} .caption-window.${langHead} {display: block !important }
+
 #movie_player.${langHead} .${langHead}-toggle::after { content: ' ON'; }
-#movie_player .${langHead}-toggle::after { content: ' OFF'; }`);
+#movie_player .${langHead}-toggle::after { content: ' OFF'; }
+
+.caption-window.${langHead} { bottom: 2%; left: 25%; right: 25%; text-align: center; font-size: 21.3333px; color: rgb(255, 255, 255); font-family: "YouTube Noto", Roboto, "Arial Unicode Ms", Arial, Helvetica, Verdana, "PT Sans Caption", sans-serif; }
+.caption-window.${langHead} .captions-text {display: inline-block; background: rgba(8, 8, 8, 0.75); fill: rgb(255, 255, 255); }
+`);
 
 },700);
 }})()
